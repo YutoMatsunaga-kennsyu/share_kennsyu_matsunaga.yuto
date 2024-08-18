78a775a9e484306e4ee6077bba4e4219968e5ffb
matsunagayuto1-admin <matsunagakennsyu@gmail.com>
2024-08-18 18:18:15 +0900

    アプリ開発（ソースコード）の新規作成（8月18日）

---
+++ アプリ開発（ソースコード）/calculator/calculator/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.calculationResult = new System.Windows.Forms.TextBox();
+            this.btnAllClear = new System.Windows.Forms.Button();
+            this.btnWaru = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnKakeru = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPoint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnEqual.FlatAppearance.BorderSize = 0;
+            this.btnEqual.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEqual.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnEqual.Location = new System.Drawing.Point(216, 430);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(96, 65);
+            this.btnEqual.TabIndex = 5;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = false;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.Window;
+            this.btn5.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn5.Location = new System.Drawing.Point(114, 288);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(96, 65);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.Window;
+            this.btn4.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn4.Location = new System.Drawing.Point(12, 288);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(96, 65);
+            this.btn4.TabIndex = 8;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.BackColor = System.Drawing.SystemColors.Window;
+            this.btnPlus.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnPlus.Location = new System.Drawing.Point(318, 359);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(96, 136);
+            this.btnPlus.TabIndex = 9;
+            this.btnPlus.Text = "＋";
+            this.btnPlus.UseVisualStyleBackColor = false;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.Window;
+            this.btn3.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn3.Location = new System.Drawing.Point(216, 359);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(96, 65);
+            this.btn3.TabIndex = 10;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.Window;
+            this.btn2.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn2.Location = new System.Drawing.Point(114, 359);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(96, 65);
+            this.btn2.TabIndex = 11;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.Window;
+            this.btn1.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn1.Location = new System.Drawing.Point(12, 359);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(96, 65);
+            this.btn1.TabIndex = 12;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.Window;
+            this.btn8.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn8.Location = new System.Drawing.Point(114, 217);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(96, 65);
+            this.btn8.TabIndex = 13;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.Window;
+            this.btn9.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn9.Location = new System.Drawing.Point(216, 217);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(96, 65);
+            this.btn9.TabIndex = 14;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.BackColor = System.Drawing.SystemColors.Window;
+            this.btnMinus.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnMinus.Location = new System.Drawing.Point(318, 288);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(96, 65);
+            this.btnMinus.TabIndex = 16;
+            this.btnMinus.Text = "－";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.Window;
+            this.btn6.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn6.Location = new System.Drawing.Point(216, 288);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(96, 65);
+            this.btn6.TabIndex = 17;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.Window;
+            this.btn7.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn7.Location = new System.Drawing.Point(12, 217);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(96, 65);
+            this.btn7.TabIndex = 18;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // calculationResult
+            // 
+            this.calculationResult.Font = new System.Drawing.Font("游ゴシック", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.calculationResult.Location = new System.Drawing.Point(29, 44);
+            this.calculationResult.MaxLength = 9;
+            this.calculationResult.Name = "calculationResult";
+            this.calculationResult.Size = new System.Drawing.Size(369, 60);
+            this.calculationResult.TabIndex = 19;
+            this.calculationResult.Text = "0";
+            this.calculationResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnAllClear
+            // 
+            this.btnAllClear.BackColor = System.Drawing.SystemColors.Window;
+            this.btnAllClear.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnAllClear.Location = new System.Drawing.Point(12, 146);
+            this.btnAllClear.Name = "btnAllClear";
+            this.btnAllClear.Size = new System.Drawing.Size(96, 65);
+            this.btnAllClear.TabIndex = 21;
+            this.btnAllClear.Text = "AC";
+            this.btnAllClear.UseVisualStyleBackColor = false;
+            this.btnAllClear.Click += new System.EventHandler(this.btnAllClear_Click);
+            // 
+            // btnWaru
+            // 
+            this.btnWaru.BackColor = System.Drawing.SystemColors.Window;
+            this.btnWaru.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnWaru.Location = new System.Drawing.Point(319, 146);
+            this.btnWaru.Name = "btnWaru";
+            this.btnWaru.Size = new System.Drawing.Size(96, 65);
+            this.btnWaru.TabIndex = 23;
+            this.btnWaru.Text = "÷";
+            this.btnWaru.UseVisualStyleBackColor = false;
+            this.btnWaru.Click += new System.EventHandler(this.btnWaru_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.SystemColors.Window;
+            this.btn0.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn0.Location = new System.Drawing.Point(12, 430);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(96, 65);
+            this.btn0.TabIndex = 24;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnKakeru
+            // 
+            this.btnKakeru.BackColor = System.Drawing.SystemColors.Window;
+            this.btnKakeru.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnKakeru.Location = new System.Drawing.Point(318, 217);
+            this.btnKakeru.Name = "btnKakeru";
+            this.btnKakeru.Size = new System.Drawing.Size(96, 65);
+            this.btnKakeru.TabIndex = 25;
+            this.btnKakeru.Text = "×";
+            this.btnKakeru.UseVisualStyleBackColor = false;
+            this.btnKakeru.Click += new System.EventHandler(this.btnKakeru_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.Window;
+            this.btnClear.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnClear.Location = new System.Drawing.Point(114, 146);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(96, 65);
+            this.btnClear.TabIndex = 26;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPoint
+            // 
+            this.btnPoint.BackColor = System.Drawing.SystemColors.Window;
+            this.btnPoint.Font = new System.Drawing.Font("游ゴシック", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnPoint.Location = new System.Drawing.Point(114, 430);
+            this.btnPoint.Name = "btnPoint";
+            this.btnPoint.Size = new System.Drawing.Size(96, 65);
+            this.btnPoint.TabIndex = 27;
+            this.btnPoint.Text = ".";
+            this.btnPoint.UseVisualStyleBackColor = false;
+            this.btnPoint.Click += new System.EventHandler(this.btnPoint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(427, 507);
+            this.Controls.Add(this.btnPoint);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnKakeru);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnWaru);
+            this.Controls.Add(this.btnAllClear);
+            this.Controls.Add(this.calculationResult);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btnEqual);
+            this.Name = "Form1";
+            this.Text = "電卓";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.TextBox calculationResult;
+        private System.Windows.Forms.Button btnAllClear;
+        private System.Windows.Forms.Button btnWaru;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnKakeru;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPoint;
+    }
+}
+
---
+++ アプリ開発（ソースコード）/calculator/calculator/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        String answer = "0";
+        String prevOperator = null;
+        //小数点ボタンフラグ
+        Boolean decimalPointFlug = false;
+        //演算子フラグ
+        Boolean operatorFlug = false;
+
+        decimal calcFirstNum = 0;
+        decimal calcSecondNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //数字ボタン(0～9)押下時の処理
+        private void concatNum(String btnNum)
+        {
+            if (answer == "0")
+            {
+                //計算結果表示欄の値が「0」の時、「押下した数字ボタンの値」で上書きする。
+                answer = btnNum;
+            }
+            else
+            {
+                //計算結果表示欄の値が「0」以外の時、計算結果表示欄に表示されている値に、「押下した数字ボタンの値」を結合する。
+                answer = answer + btnNum;
+            }
+
+            //計算結果表示欄のTextプロパティに、値を設定する。
+            calculationResult.Text = answer;
+            //演算子フラグをtrueにする。（演算子ボタンを連続して押下した際に、何も処理を起こさないようにする）
+            if (operatorFlug == false)
+            {
+                operatorFlug = true;
+            }
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            concatNum("1");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            concatNum("2");
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            concatNum("3");
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            concatNum("4");
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            concatNum("5");
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            concatNum("6");
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            concatNum("7");
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            concatNum("8");
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            concatNum("9");
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            concatNum("0");
+        }
+
+        private void btnAllClear_Click(object sender, EventArgs e)
+        {
+            //演算子をnullにする
+            prevOperator = null;
+            answer = "0";
+            calculationResult.Text = answer;
+            calcFirstNum = 0;
+            calcSecondNum = 0;
+            operatorFlug = false;
+        }
+
+        //クリアボタン押下時の処理
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            answer = "0";
+            calculationResult.Text = answer;
+        }
+
+        //演算系ボタン（+,-,*,/,=）の処理
+        private void calculation(String clacOperator)
+        {
+            //事前に演算系ボタンを押下しているかどうかを判定
+            if (prevOperator != null)
+            {
+                //検索結果表示欄の値をセットする
+                calcSecondNum = decimal.Parse(calculationResult.Text);
+                //各演算子毎の計算
+                if (prevOperator.Equals("+"))
+                {
+                    calculationResult.Text = (calcFirstNum + calcSecondNum).ToString();
+                }
+                else if (prevOperator.Equals("-"))
+                {
+                    calculationResult.Text = (calcFirstNum - calcSecondNum).ToString();
+                }
+                else if (prevOperator.Equals("*"))
+                {
+                    calculationResult.Text = (calcFirstNum * calcSecondNum).ToString();
+                }
+                else if (prevOperator.Equals("/"))
+                {
+                    try
+                    {
+                        calculationResult.Text = (calcFirstNum / calcSecondNum).ToString();
+                    }
+                    catch (DivideByZeroException e)
+                    {
+                        //0除算した際の処理
+                        calculationResult.Text = "Error";
+                        answer = "0";
+                        return;
+                    }
+                }
+            }
+
+            //等号ボタン以外の処理
+            if (clacOperator.Equals("="))
+            {
+                //演算子の値を消去する
+                prevOperator = null;
+            }
+            else
+            {
+                //演算子フラグをfalseにする。
+                operatorFlug = false;
+                //演算子の値を格納する。
+                prevOperator = clacOperator;
+            }
+
+            //演算結果を、次回の演算に使用するために変数に格納
+            calcFirstNum = decimal.Parse(calculationResult.Text);
+            answer = "0";
+            //小数点ボタンフラグをfalseにする
+            decimalPointFlug = false;
+        }
+        private void btnWaru_Click(object sender, EventArgs e)
+        {
+            if (operatorFlug == true)
+            {
+                calculation("/");
+            }
+
+        }
+
+        private void btnKakeru_Click(object sender, EventArgs e)
+        {
+            if (operatorFlug == true)
+            {
+                calculation("*");
+            }
+
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (operatorFlug == true)
+            {
+                calculation("-");
+            }
+
+        }
+        //加算ボタンの処理
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (operatorFlug == true)
+            {
+                calculation("+");
+            }
+
+        }
+        //等号ボタンの処理
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (operatorFlug == true)
+            {
+                calculation("=");
+            }
+        }
+        //小数点ボタンの処理
+        private void btnPoint_Click(object sender, EventArgs e)
+        {
+            //計算結果表示欄の数値が小数点ボタンを押下されることで、少数になったかどうかを判定
+            if (decimalPointFlug == false)
+            {
+                answer = answer + ".";
+                calculationResult.Text = answer;
+                decimalPointFlug = true;
+            }
+        }
+    }
+}
