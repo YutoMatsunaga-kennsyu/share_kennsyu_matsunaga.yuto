946398785f09f6cb8ac99388d530d9ee67866436
matsunagayuto1-admin <matsunagakennsyu@gmail.com>
2025-03-09 11:31:52 +0900

    テーブル定義書の新規作成（3月9日）

---
+++ STEP2（タスク管理アプリ）/テーブル定義書/DDL/task_tags_DDL.sql
@@ -0,0 +1,5 @@
+CREATE TABLE task_tags (
+  tag_name VARCHAR(15) NOT NULL,
+  PRIMARY KEY(tag_name)
+);
+
---
+++ STEP2（タスク管理アプリ）/テーブル定義書/DDL/tasks_DDL.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tasks (
+  user_no int NOT NULL,
+  task_name VARCHAR(15) NOT NULL,
+  description VARCHAR(20),
+  tag VARCHAR(15) NOT NULL,
+  due_date DATE NOT NULL,
+  done_date DATETIME,
+  update_date DATETIME NOT NULL,
+  is_active boolean NOT NULL,
+  PRIMARY KEY(user_no,task_name),
+  FOREIGN KEY (user_no) REFERENCES users(user_no)
+);
+
