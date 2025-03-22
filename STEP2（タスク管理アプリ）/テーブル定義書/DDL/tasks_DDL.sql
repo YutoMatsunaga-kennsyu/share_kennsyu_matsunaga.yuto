ae70b434c13031650dc6a907ef9de90383b0b175
matsunagayuto1-admin <matsunagakennsyu@gmail.com>
2025-03-22 10:29:05 +0900

    再レビュー指摘対応（3月22日）

--- STEP2（タスク管理アプリ）/テーブル定義書/DDL/tasks_DDL.sql
+++ STEP2（タスク管理アプリ）/テーブル定義書/DDL/tasks_DDL.sql
@@ -1,13 +1,15 @@
 CREATE TABLE tasks (
+  task_no int NOT NULL AUTO_INCREMENT,
   user_no int NOT NULL,
   task_name VARCHAR(15) NOT NULL,
   description VARCHAR(30),
-  tag VARCHAR(15) NOT NULL,
+  tag_no int NOT NULL,
   due_date DATE NOT NULL,
   done_date DATETIME,
   update_date DATETIME NOT NULL,
   is_active boolean NOT NULL,
-  PRIMARY KEY(user_no,task_name),
-  FOREIGN KEY (user_no) REFERENCES users(user_no)
+  PRIMARY KEY(task_no),
+  FOREIGN KEY (user_no) REFERENCES users(user_no),
+  FOREIGN KEY (tag_no) REFERENCES task_tags(tag_no)
 );
 
