4aeadd4804da083d6707d3e833eabca2a52dc409
matsunagayuto1-admin <matsunagakennsyu@gmail.com>
2025-07-08 15:47:27 +0900

    タスク管理アプリのWebアプリ化：Tailwind CSS（7月8日）

---
+++ STEP2（タスク管理アプリ）/アプリ開発（ソースコード）/TaskManagementWeb/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.razor",
+    "./wwwroot/index.html",
+    "./**/*.html",
+    "./**/*.cshtml",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
